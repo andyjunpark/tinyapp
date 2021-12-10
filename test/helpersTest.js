4558965542c4bc08229f69881faa93607ed33752
Andy Junhyuk Park <pos03142ap@gmail.com>
2021-12-10 06:02:23 +0000

    implemented test functions for helper functions with mocha chai

---
+++ test/helpersTest.js
@@ -0,0 +1,29 @@
+const { assert } = require('chai');
+
+const { checkIfRegistered } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = checkIfRegistered("user@example.com", testUsers)
+    const expectedUserID = "userRandomID";
+    // Write your assert statement here
+    assert.equal(user, expectedUserID);
+  })
+  it('should return undefined if the email is non-existent', function() {
+    const nonUser = checkIfRegistered("abcd@example.com", testUsers)
+    assert.equal(nonUser, undefined);
+  })
+});
